97416795e89d7d242ce1e7aeef562754dafe598d
cyyoon0311 <cy_0311@naver.com>
2025-05-22 18:38:23 +0900

    feat/지도 띄우기 성공, PolylineOverlay 테스트

--- screens/courseDetail.jsx
+++ screens/courseDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { NaverMapView } from '@mj-studio/react-native-naver-map';
+import { NaverMapView, NaverMapPolylineOverlay } from '@mj-studio/react-native-naver-map';
 
 const INITIAL_CAMERA = {
   latitude: 37.5666102,
@@ -9,14 +9,25 @@ const INITIAL_CAMERA = {
   zoom: 12,
 };
 
+const routeCoordinates = [
+  { latitude: 37.5666102, longitude: 126.9783881 },
+  { latitude: 37.5703772, longitude: 126.9830465 },
+  { latitude: 37.5759286, longitude: 126.9768496 },
+];
+
 const MountainMapScreen = () => {
   return (
     <SafeAreaView edges={['top']} style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
-        <NaverMapView
-          style={{ flex: 1 }}
-          initialCamera={INITIAL_CAMERA} // 또는 center={...}
-        />
+        <NaverMapView style={{ flex: 1 }} initialCamera={INITIAL_CAMERA}>
+          <NaverMapPolylineOverlay
+            coords={routeCoordinates}
+            width={10}
+            color="#FF0000"
+            outlineWidth={2}
+            outlineColor="#000000"
+          />
+        </NaverMapView>
       </View>
     </SafeAreaView>
   );
